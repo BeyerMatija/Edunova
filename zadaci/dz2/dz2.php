2e7053a1c77f9b66346614593f24e2e168135c6f
BeyerMatija <beyermatija@gmail.com>
2017-06-04 17:28:08 +0200

    Add files via upload

---
+++ zadaci/dz2/dz2.php
@@ -0,0 +1,14 @@
+<?php 
+$d1 = $_GET["d1"];
+$d2 = $_GET["d2"];
+$d3 = $_GET["d3"];
+$d4 = $_GET["d4"];
+$rez = $d1 + $d2 + $d3 * $d4;
+?>
+
+
+
+<?php echo "Rezultat" ?>
+
+<?php echo $rezultat ?>
+	

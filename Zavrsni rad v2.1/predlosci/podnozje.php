3eef12d0c77a892d51de3a99c314847ef06bace7
BeyerMatija <beyermatija@gmail.com>
2017-08-18 12:39:16 +0200

    Add files via upload

---
+++ Zavrsni rad v2.1/predlosci/podnozje.php
@@ -0,0 +1,13 @@
+<div class="row" style="max-width: 100rem; text-align: center;">
+	<hr />
+	   &copy; Matija Beyer 2017
+	
+	<?php 
+	
+	if($_SERVER["HTTP_HOST"]==="localhost"){
+		echo ", <span style=\"color: red\">Lokalno</span>";
+	}
+	
+	?>
+	
+</div>

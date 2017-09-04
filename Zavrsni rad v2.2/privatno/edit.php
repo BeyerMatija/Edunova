bec47d8ebd5221e9c04c696a1b7efc1606bbf924
BeyerMatija <beyermatija@gmail.com>
2017-09-04 14:43:56 +0200

    Delete edit.php

--- Zavrsni rad v2.2/privatno/edit.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../../konfiguracija.php';
-//uzimamo podatke iz post metode i kreiramo upit
-$izraz= $veza->prepare("update lijek set naziv=:naziv, cijena=:cijena where sifra=:sifra");
-//nakon sto smo pripremili upi izvršimo ga s nasim parametrima
-$izraz->execute(array("naziv" => $_POST['naziv'], "cijena" => $_POST["cijena"],"sifra" => $_POST['sifra']));
-//nakon uspješnog brisanja vrati nas na listu lijekova
-header("Location: index.php");

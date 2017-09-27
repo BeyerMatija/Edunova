d5d00d76ef71eff2d9959e9af8d4115c1bcad31d
BeyerMatija <beyermatija@gmail.com>
2017-09-27 16:10:48 +0200

    Update menu.php

--- Zavrsni rad v2.2/predlosci/menu.php
+++ Zavrsni rad v2.2/predlosci/menu.php
@@ -3,7 +3,7 @@
   <div class="top-bar-left">
     <ul class="dropdown menu" data-dropdown-menu>
       <li class="menu-text"></li>
-      <li><a href="<?php echo $putanjaAPP; ?>">Početna</a></li>
+      <li><a href="<?php echo $putanjaAPP; ?>"<i title="Početna stranica" class="step fi-home size-48"></i></a></li>
       <?php
       
       	if(isset($_SESSION['user']) != 0)
@@ -11,40 +11,40 @@
       	?>
       <li>
       	
-        <a href="#">Lijekovi</a>
+        <a href="#"><i title="Programi" class="fi-first-aid size-48"></i><span class="hide-for-small-only"> </span></a>
         <ul class="menu vertical">
-		  <li><a href="<?php echo $putanjaAPP; ?>privatno/lijekovi/">Svi lijekovi</a></li>
-          <li><a href="#">Na recept</a></li>                  
+		  <li><a href="<?php echo $putanjaAPP; ?>privatno/lijekovi/">Lijekovi</a></li>
+          <li><a href="<?php echo $putanjaAPP; ?>privatno/korisnici/">Korisnici</a></li> 
+          <li><a href="<?php echo $putanjaAPP; ?>privatno/operateri/">Operateri</a></li>
+          <li><a href="<?php echo $putanjaAPP; ?>privatno/podignuto/">Podignuti lijekovi</a></li>
+          <li><a href="<?php echo $putanjaAPP; ?>privatno/posjeta/">Posjeta</a></li>                   
         </ul>
          
      </li>
-      
-      <?php 
+     
+     <?php 
 		}
-        ?>
-        
-       <li><a href="<?php echo $putanjaAPP; ?>javno/login.php">Prijava</a></li>          
+        ?>                
                
-       <li><a href="http://www.hzzo.hr/">Hzzo</a></li>
+       <li><a href="http://www.hzzo.hr/" target="_blank"><i title="Hzzo" class="fi-link size-48"></i></a> </li>
        
-       <li><a href="<?php echo $putanjaAPP; ?>javno/Onama.php">O nama</a></li>      
+       <li><a href="<?php echo $putanjaAPP; ?>javno/Onama.php"><i title="O nama" class="fi-compass size-48"></i></a></li>      
             
-       <li><a href="<?php echo $putanjaAPP; ?>img/era.png">Era</a></li>      
+       <li><a href="<?php echo $putanjaAPP; ?>img/era.png" target="_blank"><i title="ERA" class="fi-graph-bar size-48"></i></a></li>      
       
-       <li><a href="https://github.com/BeyerMatija/Edunova/tree/master/Zavrsni%20rad%20v2.2">Git</a>     	
+       <li><a href="https://github.com/BeyerMatija/Edunova/tree/master/Zavrsni%20rad%20v2.2" target="_blank"><i title="Github" class="fi-widget size-48"></i></a>     	
       	      
       <?php
       	if(isset($_SESSION['user']) != 0){
       	?>
-      	<li><a href="<?php echo $putanjaAPP; ?>javno/logout.php">Odjava</a></li>
+      	<li><a href="<?php echo $putanjaAPP; ?>javno/logout.php"><i title="Odjava" class="fi-power size-48"></i></a></li>
       	<?php
 		}else{ ?>
-      
+      <li><a href="<?php echo $putanjaAPP; ?>javno/login.php">Prijava</a></li>       
       <?php
 		}
 		?>
-     
-    </ul>
+     </ul>
   </div>
   <div class="top-bar-right">
     <ul class="menu">

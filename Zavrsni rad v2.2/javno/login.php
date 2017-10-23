968b9dcf4918a3140d02e4a8fa42ee9a20cf7915
BeyerMatija <beyermatija@gmail.com>
2017-10-23 16:33:38 +0200

    Update login.php

--- Zavrsni rad v2.2/javno/login.php
+++ Zavrsni rad v2.2/javno/login.php
@@ -1,38 +1,46 @@
-<?php include_once '../konfiguracija.php';  ?>
+<?php include_once '../konfiguracija.php'; ?>
+
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
   <head>
-    <?php include_once '../predlosci/head.php'; ?>
+    <?php include_once '../predlosci/head.php';?>
   </head>
   <body>
-  	<?php include_once '../predlosci/menu.php'; ?>
+  	<?php include_once '../predlosci/menu.php';?>
   	
-    <div class="grid-container">
-    	
-      <div class="row">
-      	
-        <div class="large-6 medium 6 cell">
-        	 <br><br>
-        	  	        	
+        <div class ="grid-x">
+  		<div class="medium-3 large-3 cell"></div>
+  		<div class="medium-6 large-6 cell">
+  			
+  			<!-- <?php
+             if(isset($_GET["nemateOvlasti"])){
+                echo "<h1>Nemate ovlasti</h1>";
+            }
+           ?> -->
+  				  	        	
 			  <form class="log-in-form" method="post" action="<?php echo $putanjaAPP;?>javno/dologin.php">
 			  	<?php 
-			  	if(isset($_GET['error']) && $_GET['error'] == 'true'){
+			  	if(isset($_GET['error']) && $_GET['error'] == 'true')				
+					
 			  		?>
-			  	
-			  <h4 class="text-center" style="color: red">PODACI NISU ISPRAVNI</h4>
-			  <?php
-				} ?>
-			  <h4 class="text-center">Prijavite se sa vašim korisničkim podacima</h4>
-			  <label>Korisničko ime
+			  				
+			  <h4 class="text-center"><b>Prijavite se sa vašim korisničkim podacima</b></h4>
+			  
+			  <label>Korisničko ime:
 			    <input type="text" name="korisnicko_ime" placeholder="korisničko ime">
 			  </label>
-			  <label>Lozinka
+			  
+			  <label>Lozinka:
 			    <input type="password" name="lozinka" placeholder="lozinka">
 			  </label>
-		
+			  
 			  <p><input type="submit" class="button expanded" value="Prijava"></input></p>
-		
-			</form>
+			  
+			  </form>
+			  
+			   <a href="<?php echo $putanjaAPP;?>index.php">
+			  <button class="alert button expanded" name="odustani">Odustani</button>
+				</a>
     	
          </div>
     </div>

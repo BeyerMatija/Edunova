0b28fce8f87e1ccd4b4dab21435add5b22deb6db
BeyerMatija <beyermatija@gmail.com>
2017-10-23 16:33:15 +0200

    Update dologin.php

--- Zavrsni rad v2.2/javno/dologin.php
+++ Zavrsni rad v2.2/javno/dologin.php
@@ -1,12 +1,19 @@
 <?php
+
 include_once '../konfiguracija.php';
-$izraz=$veza->prepare("select operater.*, korisnik.ime as ime from operater left join korisnik on korisnik.sifra = operater.korisnik where korisnicko_ime=:korisnicko_ime and lozinka=:lozinka");
+
+$izraz=$veza->prepare("select operater.*,korisnik.ime as ime,korisnik.prezime as prezime from operater left join korisnik on korisnik.sifra = operater.korisnik where korisnicko_ime=:korisnicko_ime and lozinka=md5(:lozinka)");
 //različitom mi se zovu POST parametri od parametara upita pa ih mapiram putem asocijativnog niza
 $izraz->execute(array("korisnicko_ime"=>$_POST["korisnicko_ime"], "lozinka" =>$_POST["lozinka"]));
 //dohvatim operatera
 $operater = $izraz->fetch(PDO::FETCH_OBJ);
 if($operater){
-	$_SESSION['user'] = $operater-> ime;
+
+	if($operater->isAdmin == 1){
+		$_SESSION["admin"]=1;
+	}
+	$_SESSION['user'] = $operater-> ime ." ". $operater->prezime;
+	$_SESSION["user_id"] = $operater->sifra;
 	header("Location: ../index.php");
 }else{
 	header("Location: ../javno/login.php?error=true");

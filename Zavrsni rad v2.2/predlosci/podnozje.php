df350610858c566d141f189d8ffd5b4b6c5439b4
BeyerMatija <beyermatija@gmail.com>
2017-10-25 15:17:44 +0200

    Update podnozje.php

--- Zavrsni rad v2.2/predlosci/podnozje.php
+++ Zavrsni rad v2.2/predlosci/podnozje.php
@@ -1,11 +1,11 @@
-<div class="row" style="max-width: 100rem; text-align: center;">
+<div class="row" style="text-align: center;">
 	<hr />
 	   &copy; Matija Beyer 2017
 	
 	<?php 
 	
 	if($_SERVER["HTTP_HOST"]==="localhost"){
-		echo ", <span style=\"color: red\">Lokalno</span>";
+		echo "<span style=\"color: red\">Lokalno</span>";
 	}
 	
 	?>

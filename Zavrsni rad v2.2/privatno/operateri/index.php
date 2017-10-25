0caffc3a5c9da2a7517dbf722170afceff3924ba
BeyerMatija <beyermatija@gmail.com>
2017-10-25 15:20:23 +0200

    Update index.php

--- Zavrsni rad v2.2/privatno/operateri/index.php
+++ Zavrsni rad v2.2/privatno/operateri/index.php
@@ -1,4 +1,5 @@
  <?php include_once '../../konfiguracija.php';  ?>
+ <?php include_once '../../funkcije.php'; provjeralogin(); ?>
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
   <head>
@@ -6,23 +7,28 @@
   </head>
   <body>
   	<?php include_once '../../predlosci/menu.php'; ?>
-  	<div class="row">
-  		<div class="large-12">
-  			<a href="<?php echo $putanjaAPP. "privatno/operateri/unesi.php";?>"><button class="success button">Unesi novi</button></a>
-  			</div>
-  	</div>
+  	
+  	<div class="row grid-x margin-top-20">
+  	<div class="medium-3 large-3 columns"></div>
+  	<div class="small-12 medium-6 large-6 columns">
+  	
   	    <table>
-						<thead>
-							<tr>
-								<th>Ime</th>
-								<th>Prezime</th>
-								<th>Korisničko ime</th>
-								<th>OIB</th>
-								<th>Obriši</th>
-								<th>Promjeni</th>
+				<thead>
+					<tr>
+						<th>Ime i Prezime</th>
+						<th>Korisničko ime</th>
+						<th>Akcija</th>
 								
-							</tr>
-						</thead>
+						<?php
+								
+						if(isset($_SESSION["admin"])){
+							
+								?>
+						<th>Promjena lozike</th>
+						
+							<?php  } ?>
+					</tr>
+			</thead>
 						<tbody>
 							<?php 
 						
@@ -41,19 +47,37 @@
 							?>
 							<tr>
 								
-								
-								<td><?php echo $operater->ime; ?></td>
-								<td><?php echo $operater->prezime; ?></td>
+								<td>
+									<?php echo $operater->ime; ?>
+									<?php echo $operater->prezime; ?>
+								</td>
 								<td><?php echo $operater->korisnicko_ime; ?></td>
-								<td><?php echo $operater->oib; ?></td>
-								<td><a href="<?php echo $putanjaAPP. "privatno/operateri/delete.php?sifra=".$operater->sifra?>"><i title="Obriši" class="step fi-page-delete size-48"></i></a></td>
-								<td><a href="<?php echo $putanjaAPP. "privatno/operateri/uredi.php?sifra=".$operater->sifra?>"><i title="Uredi" class="step fi-page-edit size-48"></i></a></td>
 								
+								<td>
+									<a href="<?php echo $putanjaAPP. "privatno/operateri/uredi.php?sifra=".$operater->sifra?>"><i title="Uredi" class="step fi-page-edit size-48"></i></a>
+									<a href="<?php echo $putanjaAPP. "privatno/operateri/delete.php?sifra=".$operater->sifra?>"><i title="Obriši" class="step fi-trash size-48"></i></a>
+								</td>
+								<?php
+								if(isset($_SESSION["admin"])){
+									
+								
+								?>
+								<td> 
+									<a href="<?php echo $putanjaAPP. "privatno/operateri/passEdit.php?sifra=".$operater->sifra?>">
+										<i title="Promjeni lozinku" class="step fi-key size-48"></i></a></td>
+								<?php  }; ?>
 							</tr>
 							<?php endforeach; ?>
 						</tbody>
-					</table> 	
-       
+					</table>
+				</div>
+				<div class="small-12 medium-3 large-3 columns">
+					
+		<a href="<?php echo $putanjaAPP. "privatno/operateri/unesi.php";?>">
+			
+  		<button class="success button expanded" style="float: right; width:80%;background: #14679e; color:white; margin-right: 25px;">Unesi novi</button></a>
+  	</div>
+  </div>				
 	<?php	include_once '../../predlosci/podnozje.php'; ?>
    <?php	include_once '../../predlosci/skripte.php'; ?>
   </body>
